38aba6c15392933088df96156b11b79a44053524
Alex <alexmhh1234@gmail.com>
2021-06-28 02:24:24 +0300

    three more problems done.

---
+++ jumping_on_the_clouds.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    i = 0
+    steps = 0
+    while i < len(c):
+        if i < len(c) - 2 and c[i+2] == 0:
+            i += 2
+        else:
+            i += 1
+    return steps - 1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ repeated_strings.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    occurrences = s.count('a')
+    occurrences = occurrences * int(n/len(s))
+    n = n % len(s)
+    for i in range(n):
+        if s[i] == 'a':
+            occurrences += 1
+    return occurrences
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
